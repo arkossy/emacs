c2dabe43756c30c03932e004e7e86cf94c22326b
Zsolt Arkossy <zsolt@arkossy.com>
2020-05-10 14:51:44 +0200

    Keycast and Moody update
    Keycast and Moody update and some other minor code cleanup

--- emacs-custom-settings.el
+++ emacs-custom-settings.el
@@ -5,19 +5,48 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-faces-vector
    [default bold shadow italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#000000" "#a80000" "#005200" "#8b3800" "#0030a6" "#721045" "#005589" "#ffffff"])
  '(blink-cursor-mode nil)
  '(company-quickhelp-color-background "#4F4F4F")
  '(company-quickhelp-color-foreground "#DCDCCC")
+ '(compilation-message-face (quote default))
+ '(cua-global-mark-cursor-color "#11948b")
+ '(cua-normal-cursor-color "#596e76")
+ '(cua-overwrite-cursor-color "#a67c00")
+ '(cua-read-only-cursor-color "#778c00")
  '(custom-enabled-themes (quote (modus-operandi)))
  '(custom-safe-themes
    (quote
-    ("76c5b2592c62f6b48923c00f97f74bcb7ddb741618283bdb2be35f3c0e1030e3" "faaeaf023a321d4cf8319e6a11a3b853f72ea6795a8e97f291ee2afd2697c74f" "c04daf6d2ae13e1535f56badd1d2c3ce6fcb230424ca1facf0a9ae8d17c214f2" "a86e2a26cad48e765023cf4418e591f5b427dfecd0e77d77c11abff87b7fd75a" "fb213c95bb4c2d8d9259580ae8063ce06a7094a0ccf7a85b1a53b56c5b4d9f1c" default)))
+    ("1062ccc431da96dc93b3badf5d7bfac57dfa9754204e4240787fed8487ed3195" "4a19e4c8f5d4fa8d33ec9b1c61506f1cbd0e5c30d67117b2862e734eb39fff52" "00445e6f15d31e9afaa23ed0d765850e9cd5e929be5e8e63b114a3346236c44c" "c433c87bd4b64b8ba9890e8ed64597ea0f8eb0396f4c9a9e01bd20a04d15d358" "0fffa9669425ff140ff2ae8568c7719705ef33b7a927a0ba7c5e2ffcfac09b75" "84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" "b9e9ba5aeedcc5ba8be99f1cc9301f6679912910ff92fdf7980929c2fc83ab4d" "2074a98e21377af1c50897d4330caca2b719542bcdf9618ed3c1575c99b41363" "76c5b2592c62f6b48923c00f97f74bcb7ddb741618283bdb2be35f3c0e1030e3" "faaeaf023a321d4cf8319e6a11a3b853f72ea6795a8e97f291ee2afd2697c74f" "c04daf6d2ae13e1535f56badd1d2c3ce6fcb230424ca1facf0a9ae8d17c214f2" "a86e2a26cad48e765023cf4418e591f5b427dfecd0e77d77c11abff87b7fd75a" "fb213c95bb4c2d8d9259580ae8063ce06a7094a0ccf7a85b1a53b56c5b4d9f1c" default)))
  '(fci-rule-color "#383838")
  '(flymake-error-bitmap
    (quote
     (flymake-double-exclamation-mark modus-theme-fringe-red)))
  '(flymake-note-bitmap (quote (exclamation-mark modus-theme-fringe-cyan)))
  '(flymake-warning-bitmap (quote (exclamation-mark modus-theme-fringe-yellow)))
+ '(highlight-changes-colors (quote ("#c42475" "#5e65b6")))
+ '(highlight-symbol-colors
+   (quote
+    ("#ed7ddb24b29e" "#cd82e29fd17c" "#fc9acadfb443" "#d974d4beddd6" "#df07dfc6b349" "#f76ccd6eaf2a" "#d132db91e15a")))
+ '(highlight-symbol-foreground-color "#5d737a")
+ '(highlight-tail-colors
+   (quote
+    (("#f4eedb" . 0)
+     ("#a8b84b" . 20)
+     ("#66c1b3" . 30)
+     ("#6fa5e7" . 50)
+     ("#d6a549" . 60)
+     ("#ed6e3e" . 70)
+     ("#f46495" . 85)
+     ("#f4eedb" . 100))))
+ '(hl-bg-colors
+   (quote
+    ("#d6a549" "#ed6e3e" "#ff6243" "#f46495" "#837bdf" "#6fa5e7" "#66c1b3" "#a8b84b")))
+ '(hl-fg-colors
+   (quote
+    ("#fffce9" "#fffce9" "#fffce9" "#fffce9" "#fffce9" "#fffce9" "#fffce9" "#fffce9")))
+ '(hl-paren-colors (quote ("#11948b" "#a67c00" "#007ec4" "#5e65b6" "#778c00")))
  '(hl-todo-keyword-faces
    (quote
     (("HOLD" . "#e5f040")
@@ -41,13 +70,152 @@
  '(ibuffer-filter-group-name-face (quote dired-mark))
  '(ibuffer-marked-face (quote dired-marked))
  '(ibuffer-title-face (quote dired-header))
+ '(lsp-ui-doc-border "#5d737a")
  '(nrepl-message-colors
    (quote
     ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
+ '(org-structure-template-alist
+   (quote
+    (("s" "#+BEGIN_SRC emacs-lisp
+?
+#+END_SRC")
+     ("e" "#+BEGIN_EXAMPLE
+?
+#+END_EXAMPLE")
+     ("q" "#+BEGIN_QUOTE
+?
+#+END_QUOTE")
+     ("v" "#+BEGIN_VERSE
+?
+#+END_VERSE")
+     ("V" "#+BEGIN_VERBATIM
+?
+#+END_VERBATIM")
+     ("c" "#+BEGIN_CENTER
+?
+#+END_CENTER")
+     ("C" "#+BEGIN_COMMENT
+?
+#+END_COMMENT")
+     ("l" "#+BEGIN_EXPORT latex
+?
+#+END_EXPORT")
+     ("L" "#+LaTeX: ")
+     ("h" "#+BEGIN_EXPORT html
+?
+#+END_EXPORT")
+     ("H" "#+HTML: ")
+     ("a" "#+BEGIN_EXPORT ascii
+?
+#+END_EXPORT")
+     ("A" "#+ASCII: ")
+     ("i" "#+INDEX: ?")
+     ("I" "#+INCLUDE: %file ?"))))
+ '(package-selected-packages
+   (quote
+    (keycast moody diff-hl whitespace-cleanup-mode rainbow-mode doom-themes undo-tree rotate zenburn-theme use-package theme-looper rainbow-delimiters olivetti multiple-cursors multi-term modus-vivendi-theme modus-operandi-theme ivy autothemer auto-complete)))
  '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
+ '(pos-tip-background-color "#f4eedb")
+ '(pos-tip-foreground-color "#5d737a")
  '(show-paren-mode t)
+ '(smartrep-mode-line-active-bg (solarized-color-blend "#778c00" "#f4eedb" 0.2))
+ '(sml/mode-width
+   (if
+       (eq
+	(powerline-current-separator)
+	(quote arrow))
+       (quote right)
+     (quote full)))
+ '(sml/pos-id-separator
+   (quote
+    (""
+     (:propertize " " face powerline-active1)
+     (:eval
+      (propertize " "
+		  (quote display)
+		  (funcall
+		   (intern
+		    (format "powerline-%s-%s"
+			    (powerline-current-separator)
+			    (car powerline-default-separator-dir)))
+		   (quote powerline-active1)
+		   (quote powerline-active2))))
+     (:propertize " " face powerline-active2))))
+ '(sml/pos-minor-modes-separator
+   (quote
+    (""
+     (:propertize " " face powerline-active1)
+     (:eval
+      (propertize " "
+		  (quote display)
+		  (funcall
+		   (intern
+		    (format "powerline-%s-%s"
+			    (powerline-current-separator)
+			    (cdr powerline-default-separator-dir)))
+		   (quote powerline-active1)
+		   (quote sml/global))))
+     (:propertize " " face sml/global))))
+ '(sml/pre-id-separator
+   (quote
+    (""
+     (:propertize " " face sml/global)
+     (:eval
+      (propertize " "
+		  (quote display)
+		  (funcall
+		   (intern
+		    (format "powerline-%s-%s"
+			    (powerline-current-separator)
+			    (car powerline-default-separator-dir)))
+		   (quote sml/global)
+		   (quote powerline-active1))))
+     (:propertize " " face powerline-active1))))
+ '(sml/pre-minor-modes-separator
+   (quote
+    (""
+     (:propertize " " face powerline-active2)
+     (:eval
+      (propertize " "
+		  (quote display)
+		  (funcall
+		   (intern
+		    (format "powerline-%s-%s"
+			    (powerline-current-separator)
+			    (cdr powerline-default-separator-dir)))
+		   (quote powerline-active2)
+		   (quote powerline-active1))))
+     (:propertize " " face powerline-active1))))
+ '(sml/pre-modes-separator (propertize " " (quote face) (quote sml/modes)))
+ '(term-default-bg-color "#fffce9")
+ '(term-default-fg-color "#596e76")
  '(tool-bar-mode nil)
+ '(vc-annotate-background nil)
  '(vc-annotate-background-mode nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#a80000")
+     (40 . "#721045")
+     (60 . "#8f0075")
+     (80 . "#880000")
+     (100 . "#8b3800")
+     (120 . "#714900")
+     (140 . "#5d3026")
+     (160 . "#184034")
+     (180 . "#005200")
+     (200 . "#4a5700")
+     (220 . "#005a68")
+     (240 . "#185870")
+     (260 . "#005589")
+     (280 . "#093060")
+     (300 . "#0030a6")
+     (320 . "#223fbf")
+     (340 . "#0000bb")
+     (360 . "#5317ac"))))
+ '(vc-annotate-very-old-color nil)
+ '(weechat-color-list
+   (quote
+    (unspecified "#fffce9" "#f4eedb" "#990001" "#cc1f24" "#4f6600" "#778c00" "#785700" "#a67c00" "#005797" "#007ec4" "#93004d" "#c42475" "#006d68" "#11948b" "#596e76" "#88999b")))
  '(xterm-color-names
    ["#000000" "#ff8059" "#44bc44" "#eecc00" "#33beff" "#feacd0" "#00d3d0" "#a8a8a8"])
  '(xterm-color-names-bright
@@ -57,4 +225,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 150 :width normal :foundry "nil" :family "Hack")))))
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 150 :width normal :foundry "nil" :family "Hack"))))
+ '(erc-input-face ((t (:foreground "antique white"))))
+ '(helm-selection ((t (:background "ForestGreen" :foreground "black"))))
+ '(org-agenda-clocking ((t (:inherit secondary-selection :foreground "black"))))
+ '(org-agenda-done ((t (:foreground "dim gray" :strike-through nil))))
+ '(org-clock-overlay ((t (:background "SkyBlue4" :foreground "black"))))
+ '(org-done ((t (:foreground "PaleGreen" :weight normal :strike-through t))))
+ '(org-headline-done ((((class color) (min-colors 16) (background dark)) (:foreground "LightSalmon" :strike-through t))))
+ '(outline-1 ((t (:inherit font-lock-function-name-face :foreground "cornflower blue")))))
--- emacs-personal-settings.el
+++ emacs-personal-settings.el
@@ -1,10 +1,15 @@
-(setq inhibit-startup-screen t)
+(setq user-full-name "Zsolt Arkossy"
+      user-mail-address "zsolt@arkossy.com")
 
-(toggle-scroll-bar -1)
-
-(menu-bar-mode 1)
-
-(tool-bar-mode -1)
+(setq inhibit-startup-screen t) ; No Splash Screen
+(menu-bar-mode 1) ; No Menu Mar
+(tool-bar-mode -1) ; No Tool Bar
+(toggle-scroll-bar -1) ; No Scroll Bar
+(setq ring-bell-function 'ignore) ; No Bell Sound
+(blink-cursor-mode 0) ; No Cursor Blink
+(fset 'yes-or-no-p 'y-or-n-p) ; Set Yes/No Action
+(column-number-mode 1) ; Show Column Number in Mode Line
+(show-paren-mode 1) ; Set Matching Parenthesis
 
 (use-package emacs
   :config
@@ -12,71 +17,68 @@
     (interactive)
     (let ((configs "~/.emacs.d/emacs-personal-settings.el"))
       (when configs
-        (delete-file configs))))
+	(delete-file configs))))
   :hook (kill-emacs . apollo/delete-emacs-init))
 
-(put 'upcase-region 'disabled nil)
-(put 'downcase-region 'disabled nil)
+(setq backup-directory-alist '(("." . "~/.emacs.d/emacs-backups")))
 
-(setq sentence-end-double-space nil)
+(setq delete-old-versions -1)
+(setq version-control t)
+(setq vc-make-backup-files t)
+(setq auto-save-file-name-transforms '((".*" "~/.emacs.d/emacs-autosave/" t)))
 
-(setq sentence-end-double-space nil)
-
-(blink-cursor-mode 0)
-
-(global-visual-line-mode t)
-
-(show-paren-mode 1)
-
-(setq ring-bell-function 'ignore)
-
-(defvar backup-dir (expand-file-name "/Users/apollo/.emacs.d/emacs-backups/"))
-(defvar autosave-dir (expand-file-name "/Users/apollo/.emacs.d/emacs-autosave/"))
-(setq backup-directory-alist (list (cons ".*" backup-dir)))
-(setq auto-save-list-file-prefix autosave-dir)
-(setq auto-save-file-name-transforms `((".*" ,autosave-dir t)))
-
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
-
-(setq default-directory "~/Documents/project-emacs")
-
-(require 'ido)
-(ido-mode 1)
-(setq ido-everywhere t)
-(setq ido-enable-flex-matching t)
-(setq ido-enable-last-directory-history t)
-
-(add-to-list 'ido-ignore-files "emacs-personal-settings.el")
-(add-to-list 'ido-ignore-files ".pia_manager_crash.log")
-
-;Avoid certain directories:
-;(setq ido-ignore-directories '("Applications/" "Library/" "Movies/" "Music/" "Pictures/"))
-
-(set-face-attribute 'fringe nil :background nil)
+(setq savehist-file "~/.emacs.d/emacs-save-history")
+(savehist-mode 1)
+(setq history-length t)
+(setq history-delete-duplicates t)
+(setq savehist-save-minibuffer-history 1)
+(setq savehist-additional-variables
+      '(kill-ring
+	search-ring
+	regexp-search-ring))
 
 (require 'theme-looper)
 (theme-looper-reset-themes-selection)
 (global-set-key (kbd "C-}") 'theme-looper-enable-next-theme)
-(theme-looper-set-favorite-themes '(modus-operandi modus-vivendi))
+(theme-looper-set-favorite-themes '(modus-vivendi modus-operandi zenburn))
 
-(desktop-save-mode 1)
+(global-set-key (kbd "C-c t") 'display-current-time)
 
-;; Use CMD+arrows
-(windmove-default-keybindings 'super)
-;; Don't cycle around at edges (nil), enabled (t)
-(setq windmove-wrap-around nil)
+(defun display-current-time ()
+  (interactive)
+  (message (format-time-string "%Y-%m-%d %H:%M:%S")))
 
-(global-set-key (kbd "<s-f1>") 'split-window-below)
-(global-set-key (kbd "<s-f2>") 'split-window-right)
-(global-set-key (kbd "<s-f3>") 'balance-windows)
-(global-set-key (kbd "<s-f4>") 'delete-other-windows)
-(global-set-key (kbd "<s-f5>") 'delete-window)
+(set-face-attribute 'fringe nil :background nil)
 
-(require 'rotate)
-(global-set-key (kbd "<s-f9>") 'rotate-window)
-(global-set-key (kbd "<s-f10>") 'rotate:even-horizontal)
-(global-set-key (kbd "<s-f11>") 'rotate-layout)
-(global-set-key (kbd "<s-f12>") 'rotate:tiled)
+;; `line-spacing' is nil by default, I change it from time to time
+(setq-default line-spacing 4)
+
+(global-set-key (kbd "C-+") 'text-scale-increase)
+(global-set-key (kbd "C--") 'text-scale-decrease)
+;(global-set-key (kbd "C-=") 'text-scale-adjust)
+
+(use-package moody
+  :ensure)
+
+(use-package keycast
+  :ensure
+  :after moody
+  :commands keycast-mode
+  :config
+  (setq keycast-window-predicate 'moody-window-active-p)
+  (setq keycast-separator-width 1)
+  (setq keycast-insert-after 'mode-line-buffer-identification)
+  (setq keycast-remove-tail-elements nil))
+
+(keycast-mode 1)
+
+;; on error delete the content of: .mc-lists.el
+(require 'multiple-cursors)
+(global-set-key (kbd "M-m") 'mc/edit-lines)
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+;(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 
 (use-package olivetti
   :ensure
@@ -92,36 +94,100 @@ are disabled for the current window.  For the font-related
 changes see `prot/variable-pitch-mode'."
     (interactive)
     (if (bound-and-true-p olivetti-mode)
-        (progn
-          (olivetti-mode -1)
-          (set-window-fringes (selected-window) nil) ; Use default width
-          (prot/variable-pitch-mode))
+	(progn
+	  (olivetti-mode -1)
+	  (set-window-fringes (selected-window) nil) ; Use default width
+	  (prot/variable-pitch-mode))
       (olivetti-mode 1)
       (set-window-fringes (selected-window) 0 0)
       (prot/variable-pitch-mode (prot/reading-fonts))))
   :bind ("C-{" . prot/olivetti-mode))
 
-;; on error delete the content of: .mc-lists.el
-(require 'multiple-cursors)
-(global-set-key (kbd "M-m") 'mc/edit-lines)
-(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+(setq echo-keystrokes 0.1)
 
-(ac-config-default)
+(global-visual-line-mode t)
 
-(setq org-list-description-max-indent 5)
+(global-set-key (kbd "C-c w") 'auto-fill-mode)
+(add-hook 'text-mode-hook 'auto-fill-mode)
+(setq-default fill-column 80)
 
-(setq org-adapt-indentation nil)
+(setq sentence-end-double-space nil)
 
-(global-set-key (kbd "<f5>") 'org-id-get-create)
+(setq sentence-end-double-space nil)
+
+(put 'upcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
+
+(bind-key "C-x p" 'pop-to-mark-command)
+(setq set-mark-command-repeat-pop t)
+
+(bind-key "M-SPC" 'cycle-spacing)
+
+(add-to-list 'load-path "~/.emacs.d/additional-packages/")
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+
+(setq default-directory "~/Documents/project-emacs")
+
+(require 'ido)
+(ido-mode 1)
+(setq ido-everywhere t)
+(setq ido-enable-flex-matching t)
+(setq ido-enable-last-directory-history t)
+
+(add-to-list 'ido-ignore-files "emacs-personal-settings.el")
+(add-to-list 'ido-ignore-files ".pia_manager_crash.log")
+(add-to-list 'ido-ignore-files "archive-todo.org")
+(add-to-list 'ido-ignore-files "archive-day.org")
+;Avoid certain directories:
+;(setq ido-ignore-directories '("Applications/" "Library/" "Movies/" "Music/" "Pictures/"))
+
+(use-package undo-tree
+  :diminish undo-tree-mode
+  :config
+  (progn
+    (global-undo-tree-mode)
+    (setq undo-tree-visualizer-timestamps t)
+    (setq undo-tree-visualizer-diff t)))
+
+(desktop-save-mode 1)
+
+;; Use CMD+arrows
+(windmove-default-keybindings 'super)
+;; Don't cycle around at edges (nil), enabled (t)
+(setq windmove-wrap-around nil)
+
+(global-set-key (kbd "<s-f1>") 'split-window-below)
+(global-set-key (kbd "<s-f2>") 'split-window-right)
+(global-set-key (kbd "<s-f3>") 'balance-windows)
+(global-set-key (kbd "<s-f4>") 'delete-other-windows)
+(global-set-key (kbd "<s-f5>") 'delete-window)
+
+(require 'rotate)
+(global-set-key (kbd "<s-f12>") 'rotate-window)
+(global-set-key (kbd "<s-f11>") 'rotate:even-horizontal)
+(global-set-key (kbd "<s-f10>") 'rotate-layout)
+
+(setq org-directory "~/Documents/project-emacs")
+
+(setq org-agenda-start-on-weekday 1)
+
+(defun my/org-add-ids-to-headlines-in-file ()
+  "Add ID properties to all headlines in the current file which
+do not already have one."
+  (interactive)
+  (org-map-entries 'org-id-get-create))
+
+(add-hook 'org-mode-hook
+	  (lambda ()
+	    (add-hook 'before-save-hook 'my/org-add-ids-to-headlines-in-file nil 'local)))
+
 
 (defun my/copy-id-to-clipboard() "Copy the ID property value to killring,
-if no ID is there then create a new unique ID. 
+if no ID is there then create a new unique ID.
 This function works only in org-mode buffers.
 
-The purpose of this function is to easily construct id:-links to 
+The purpose of this function is to easily construct id:-links to
 org-mode items. If its assigned to a key it saves you marking the
 text and copying to the killring."
        (interactive)
@@ -133,25 +199,37 @@ text and copying to the killring."
 
 (global-set-key (kbd "<f6>") 'my/copy-id-to-clipboard)
 
-(setq org-directory "~/Documents/project-emacs")
+(global-set-key (kbd "<s-f6>") 'org-id-update-id-locations)
+
+(setq calendar-week-start-day 1)
+
+(setq org-todo-keyword-faces
+      (quote (("TODO" :foreground "#3a70af" :weight bold)
+	      ("NEXT" :foreground "#cc0000" :weight bold)
+	      ("DONE" :foreground "#00994d" :weight bold)
+	      ("WAITING" :foreground "#ff8833" :weight bold)
+	      ("HOLD" :foreground "#ff8833" :weight bold)
+	      ("CANCELLED" :foreground "#177a21" :weight bold))))
 
 (add-to-list 'auto-mode-alist '("\\.\\(org\\)$" . org-mode))
 (require 'org)
 
-(global-set-key "\C-cl" 'org-store-link)
-
 (global-set-key "\C-ca" 'org-agenda)
-
-(setq org-agenda-files (list "~/Documents/project-emacs"))
-
-(setq org-todo-keyword-faces
-      (quote (("TODO" :foreground "#3a70af" :weight bold)
-              ("NEXT" :foreground "#cc0000" :weight bold)
-              ("DONE" :foreground "#00994d" :weight bold)
-              ("WAITING" :foreground "#ff8833" :weight bold)
-              ("HOLD" :foreground "#ff8833" :weight bold)
-              ("CANCELLED" :foreground "#177a21" :weight bold))))
 
 (setq org-use-fast-todo-selection t)
 
 (global-set-key (kbd "C-c c") 'org-capture)
+
+(setq org-agenda-files (list "~/Documents/project-emacs"))
+
+(global-set-key "\C-cl" 'org-store-link)
+
+(setq initial-major-mode 'org-mode)
+
+(setq org-priority-faces '((?A . (:foreground "red" :weight 'bold))
+			   (?B . (:foreground "orange"))
+			   (?C . (:foreground "blue"))))
+
+(setq org-list-description-max-indent 5)
+
+(setq org-adapt-indentation nil)
